8409bfd27ed11de6596656b398709d7567489953
michellealvera <89299208+michellealvera@users.noreply.github.com>
2023-04-11 08:18:58 +0700

    Initial Commit

---
+++ 0706012110021-MichelleAlvera-AFL3/_706012110021_MichelleAlvera_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110021_MichelleAlvera_AFL3App.swift
+//  0706012110021-MichelleAlvera-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110021_MichelleAlvera_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
