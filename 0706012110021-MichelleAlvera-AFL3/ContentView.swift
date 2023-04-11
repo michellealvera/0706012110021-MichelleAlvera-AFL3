f2487f17ee4f8f7ba04b70aad8df86e6997f1ffa
michellealvera <89299208+michellealvera@users.noreply.github.com>
2023-04-11 08:49:27 +0700

    Section 3 done Combine Views Using Stacks

--- 0706012110021-MichelleAlvera-AFL3/ContentView.swift
+++ 0706012110021-MichelleAlvera-AFL3/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-          //removed green font color
-        
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+
     }
 }
 
