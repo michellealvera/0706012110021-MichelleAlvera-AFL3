ba41082fa3e97731a2dc41ea388062c40228ed93
michellealvera <89299208+michellealvera@users.noreply.github.com>
2023-04-14 09:28:32 +0700

    Section 7 - done Pass Data into Child Views

--- 0706012110021-MichelleAlvera-AFL3/Views/CircleImage.swift
+++ 0706012110021-MichelleAlvera-AFL3/Views/CircleImage.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+
     var body: some View {
-        Image("turtlerock")
-            .clipShape(Circle())
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
-                            Circle().stroke(.white, lineWidth: 4)
-                        }
+                Circle().stroke(.white, lineWidth: 4)
+            }
             .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
